2cc11ecaef6057abd50c8c6f33d235f289ba8345
李园 <liyuan05@corp.netease.com>
2021-11-19 18:31:47 +0800

    as 0 1 2

---
+++ Assignment1/code/main.cpp
@@ -0,0 +1,177 @@
+#include "Triangle.hpp"
+#include "rasterizer.hpp"
+#include <Eigen/Eigen>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+constexpr double MY_PI = 3.1415926;
+
+Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
+{
+    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f translate;
+    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
+        -eye_pos[2], 0, 0, 0, 1;
+
+    view = translate * view;
+
+    return view;
+}
+
+Eigen::Matrix4f get_model_matrix(float rotation_angle)
+{
+    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+
+    float angle = rotation_angle / 180.0f * MY_PI;
+    float sinA = sin(angle);
+    float cosA = cos(angle);
+    Eigen::Matrix4f translate;
+    translate << cosA, -sinA, 0, 0,
+        sinA, cosA, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
+
+    model = translate;
+
+    return model;
+}
+
+Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
+                                      float zNear, float zFar)
+{
+    // Students will implement this function
+
+    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
+
+    Eigen::Matrix4f persp2ortho(4, 4);
+    Eigen::Matrix4f ortho_scale(4, 4);
+    Eigen::Matrix4f ortho_trans(4, 4);
+
+    float angle = eye_fov / 2 / 180.0f * MY_PI;
+    float n = zNear;
+    float f = zFar;
+
+    auto t = -n * tan(angle); 
+    auto r = t * aspect_ratio;
+    auto b = -t;
+    auto l = -r;
+
+    persp2ortho << n, 0, 0, 0,
+        0, n, 0, 0,
+        0, 0, n + f, -(n * f),
+        0, 0, 1, 0;
+
+    ortho_trans << 1, 0, 0, -(r + l) / 2,
+        0, 1, 0, -(t + b) / 2,
+        0, 0, 1, -(n + f) / 2,
+        0, 0, 0, 1;
+
+    ortho_scale << 2 / (r - l), 0, 0, 0,
+        0, 2 / (t - b), 0, 0,
+        0, 0, 2 / (n - f), 0,
+        0, 0, 0, 1;
+
+    projection = ortho_scale * ortho_trans * persp2ortho;
+
+    return projection;
+}
+
+Eigen::Matrix4f get_rotation(Eigen::Vector4f axis, float rotation_angle)
+{
+    rotation_angle = rotation_angle / 180.0f * MY_PI;
+    Eigen::Matrix4f rotation = Eigen::Matrix4f::Identity();
+    Eigen::Matrix4f tmp;
+    float cosA = std::cos(rotation_angle);
+    float sinA = std::sin(rotation_angle);
+    tmp<< 0,-axis[2], axis[1], 0,
+        axis[2], 0, -axis[0], 0,
+        -axis[1], axis[0], 0, 0,
+        0, 0, 0, 1;
+    rotation = cosA * rotation + (1 - cosA) * axis * axis.transpose() + sinA * tmp;
+    return rotation;
+}
+
+int main(int argc, const char **argv)
+{
+    float angle = 0;
+    bool command_line = false;
+    std::string filename = "output.png";
+
+    if (argc >= 3)
+    {
+        command_line = true;
+        angle = std::stof(argv[2]); // -r by default
+        if (argc == 4)
+        {
+            filename = std::string(argv[3]);
+        }
+        else
+            return 0;
+    }
+
+    rst::rasterizer r(700, 700);
+
+    Eigen::Vector3f eye_pos = {0, 0, 5};
+
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+
+    auto pos_id = r.load_positions(pos);
+    auto ind_id = r.load_indices(ind);
+
+    int key = 0;
+    int frame_count = 0;
+    
+    //实现绕任意轴旋转
+    Eigen::Vector4f a(0, 0, 1, 0);
+
+    if (command_line)
+    {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        // r.set_model(get_rotation(a, angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+
+        cv::imwrite(filename, image);
+
+        return 0;
+    }
+
+    while (key != 27)
+    {
+        r.clear(rst::Buffers::Color | rst::Buffers::Depth);
+
+        r.set_model(get_model_matrix(angle));
+        // r.set_model(get_rotation(a, angle));
+        r.set_view(get_view_matrix(eye_pos));
+        r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
+
+        r.draw(pos_id, ind_id, rst::Primitive::Triangle);
+
+        cv::Mat image(700, 700, CV_32FC3, r.frame_buffer().data());
+        image.convertTo(image, CV_8UC3, 1.0f);
+        cv::imshow("image", image);
+        key = cv::waitKey(10);
+
+        std::cout << "frame count: " << frame_count++ << '\n';
+
+        if (key == 'a')
+        {
+            angle += 10;
+        }
+        else if (key == 'd')
+        {
+            angle -= 10;
+        }
+    }
+
+    return 0;
+}
---
+++ Assignment2/code/rasterizer.cpp
@@ -0,0 +1,248 @@
+// clang-format off
+//
+// Created by goksu on 4/6/19.
+//
+
+#include <algorithm>
+#include <vector>
+#include "rasterizer.hpp"
+#include <opencv2/opencv.hpp>
+#include <math.h>
+
+
+rst::pos_buf_id rst::rasterizer::load_positions(const std::vector<Eigen::Vector3f> &positions)
+{
+    auto id = get_next_id();
+    pos_buf.emplace(id, positions);
+
+    return {id};
+}
+
+rst::ind_buf_id rst::rasterizer::load_indices(const std::vector<Eigen::Vector3i> &indices)
+{
+    auto id = get_next_id();
+    ind_buf.emplace(id, indices);
+
+    return {id};
+}
+
+rst::col_buf_id rst::rasterizer::load_colors(const std::vector<Eigen::Vector3f> &cols)
+{
+    auto id = get_next_id();
+    col_buf.emplace(id, cols);
+
+    return {id};
+}
+
+auto to_vec4(const Eigen::Vector3f& v3, float w = 1.0f)
+{
+    return Vector4f(v3.x(), v3.y(), v3.z(), w);
+}
+
+
+static bool insideTriangle(double x, double y, const Vector3f* _v)
+{   
+    Eigen::Vector2f p(x, y);
+
+    Eigen::Vector2f AB = _v[1].head(2) - _v[0].head(2);
+    Eigen::Vector2f BC = _v[2].head(2) - _v[1].head(2);
+    Eigen::Vector2f CA = _v[0].head(2) - _v[2].head(2);
+
+    Eigen::Vector2f AP = p - _v[0].head(2);
+    Eigen::Vector2f BP = p - _v[1].head(2);
+    Eigen::Vector2f CP = p - _v[2].head(2);
+    
+    return AB[0] * AP[1] - AB[1] * AP[0] > 0 && BC[0] * BP[1] - BC[1] * BP[0] > 0 && CA[0] * CP[1] - CA[1] * CP[0] > 0;
+    
+}
+
+static std::tuple<float, float, float> computeBarycentric2D(float x, float y, const Vector3f* v)
+{
+    float c1 = (x*(v[1].y() - v[2].y()) + (v[2].x() - v[1].x())*y + v[1].x()*v[2].y() - v[2].x()*v[1].y()) / (v[0].x()*(v[1].y() - v[2].y()) + (v[2].x() - v[1].x())*v[0].y() + v[1].x()*v[2].y() - v[2].x()*v[1].y());
+    float c2 = (x*(v[2].y() - v[0].y()) + (v[0].x() - v[2].x())*y + v[2].x()*v[0].y() - v[0].x()*v[2].y()) / (v[1].x()*(v[2].y() - v[0].y()) + (v[0].x() - v[2].x())*v[1].y() + v[2].x()*v[0].y() - v[0].x()*v[2].y());
+    float c3 = (x*(v[0].y() - v[1].y()) + (v[1].x() - v[0].x())*y + v[0].x()*v[1].y() - v[1].x()*v[0].y()) / (v[2].x()*(v[0].y() - v[1].y()) + (v[1].x() - v[0].x())*v[2].y() + v[0].x()*v[1].y() - v[1].x()*v[0].y());
+    return {c1,c2,c3};
+}
+
+void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type)
+{
+    auto& buf = pos_buf[pos_buffer.pos_id];
+    auto& ind = ind_buf[ind_buffer.ind_id];
+    auto& col = col_buf[col_buffer.col_id];
+
+    float f1 = (50 - 0.1) / 2.0;
+    float f2 = (50 + 0.1) / 2.0;
+
+    Eigen::Matrix4f mvp = projection * view * model;
+    for (auto& i : ind)
+    {
+        Triangle t;
+        Eigen::Vector4f v[] = {
+                mvp * to_vec4(buf[i[0]], 1.0f),
+                mvp * to_vec4(buf[i[1]], 1.0f),
+                mvp * to_vec4(buf[i[2]], 1.0f)
+        };
+        //Homogeneous division
+        for (auto& vec : v) {
+            vec /= vec.w();
+        }
+        //Viewport transformation
+        for (auto & vert : v)
+        {
+            vert.x() = 0.5*width*(vert.x()+1.0);
+            vert.y() = 0.5*height*(vert.y()+1.0);
+            vert.z() = vert.z() * f1 + f2;
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            t.setVertex(i, v[i].head<3>());
+            t.setVertex(i, v[i].head<3>());
+            t.setVertex(i, v[i].head<3>());
+        }
+
+        auto col_x = col[i[0]];
+        auto col_y = col[i[1]];
+        auto col_z = col[i[2]];
+
+        t.setColor(0, col_x[0], col_x[1], col_x[2]);
+        t.setColor(1, col_y[0], col_y[1], col_y[2]);
+        t.setColor(2, col_z[0], col_z[1], col_z[2]);
+        rasterize_triangle(t);
+    }
+}
+
+//Screen space rasterization
+void rst::rasterizer::rasterize_triangle(const Triangle& t) {
+    auto v = t.toVector4();
+			   
+    //bounding box
+    float min_x = std::min(v[0][0], std::min(v[1][0], v[2][0]));
+    float max_x = std::max(v[0][0], std::max(v[1][0], v[2][0]));
+    float min_y = std::min(v[0][1], std::min(v[1][1], v[2][1]));
+    float max_y = std::max(v[0][1], std::max(v[1][1], v[2][1]));
+
+    min_x = static_cast<int>(std::floor(min_x));
+    min_y = static_cast<int>(std::floor(min_y));
+    max_x = static_cast<int>(std::ceil(max_x));
+    max_y = static_cast<int>(std::ceil(max_y));
+
+    bool MSAA = true;//MSAA是否启用
+
+    if (MSAA)
+    {
+        std::vector<Eigen::Vector2f> pos
+        {                               //对一个像素分割四份 当然你还可以分成4x4 8x8等等甚至你还可以为了某种特殊情况设计成不规则的图形来分割单元
+            {0.25,0.25},                //左下
+            {0.75,0.25},                //右下
+            {0.25,0.75},                //左上
+            {0.75,0.75}                 //右上
+        };
+        for (int i = min_x; i <= max_x; ++i)
+        {
+            for (int j = min_y; j <= max_y; ++j)
+            {
+                int count = 0;
+                float minDepth = FLT_MAX;
+                for (int MSAA_4 = 0; MSAA_4 < 4; ++MSAA_4)
+                {
+                    if (insideTriangle(static_cast<float>(i+pos[MSAA_4][0]), static_cast<float>(j+pos[MSAA_4][1]),t.v))
+                    {
+			            auto[alpha, beta, gamma] = computeBarycentric2D(static_cast<float>(i + pos[MSAA_4][0]), static_cast<float>(j + pos[MSAA_4][1]), t.v);
+	                    float w_reciprocal = 1.0/(alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+	                    float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
+	                    z_interpolated *= w_reciprocal;
+
+                        minDepth = std::min(minDepth, z_interpolated);
+                        ++count;
+                    }
+                }
+                if (count)
+                {
+                    if (depth_buf[get_index(i, j)] > minDepth)
+                    {
+                        depth_buf[get_index(i, j)] = minDepth;
+
+                        Eigen::Vector3f color = t.getColor() * (count / 4.0);//对颜色进行平均，使得边界更平滑，也是一种模糊的手段
+                        Eigen::Vector3f point;
+                        point << static_cast<float>(i), static_cast<float>(j), minDepth;
+                        set_pixel(point, color);
+                    }
+                }
+            }
+        }
+    }
+    else
+    {   
+		for (int i = min_x; i <= max_x; ++i)
+		{
+			for (int j = min_y; j <= max_y; ++j)
+			{
+                if (insideTriangle(float(i+0.5), float(j+0.5) ,t.v))
+				{
+					auto [alpha, beta, gamma] = computeBarycentric2D(float(i+0.5), float(j+0.5), t.v);
+					float w_reciprocal = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+					float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
+					z_interpolated *= w_reciprocal;
+
+					if (depth_buf[get_index(i, j)] > z_interpolated)
+					{
+						depth_buf[get_index(i, j)] = z_interpolated;
+						Eigen::Vector3f color = t.getColor();
+						Eigen::Vector3f point;
+						point << static_cast<float>(i), static_cast<float>(j), z_interpolated;
+						set_pixel(point, color);
+					}
+				}
+			}
+		}
+	}
+}
+
+void rst::rasterizer::set_model(const Eigen::Matrix4f& m)
+{
+    model = m;
+}
+
+void rst::rasterizer::set_view(const Eigen::Matrix4f& v)
+{
+    view = v;
+}
+
+void rst::rasterizer::set_projection(const Eigen::Matrix4f& p)
+{
+    projection = p;
+}
+
+void rst::rasterizer::clear(rst::Buffers buff)
+{
+    if ((buff & rst::Buffers::Color) == rst::Buffers::Color)
+    {
+        std::fill(frame_buf.begin(), frame_buf.end(), Eigen::Vector3f{0, 0, 0});
+    }
+    if ((buff & rst::Buffers::Depth) == rst::Buffers::Depth)
+    {
+        std::fill(depth_buf.begin(), depth_buf.end(), std::numeric_limits<float>::infinity());
+    }
+}
+
+rst::rasterizer::rasterizer(int w, int h) : width(w), height(h)
+{
+    frame_buf.resize(w * h);
+    depth_buf.resize(w * h);
+}
+
+int rst::rasterizer::get_index(int x, int y)
+{
+    return (height-1-y)*width + x;
+}
+
+void rst::rasterizer::set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color)
+{
+    //old index: auto ind = point.y() + point.x() * width;
+    auto ind = (height-1-point.y())*width + point.x();
+    frame_buf[ind] = color;
+
+}
+
+// clang-format on
